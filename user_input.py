8253aa549889b7d9410474a34b6a251248922f0b
Your Name <kkamadi567@gmail.com>
2024-03-11 04:13:18 +0300

    first

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# user name
+name =input("Enter your name: ")
+# user age
+age=input("Enter your age: ")
+# user city
+city=input("Enter your city: ")
+print(f"Hello, {name}! Your age is {age} and you are from {city}.")
